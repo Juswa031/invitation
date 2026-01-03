481f66588ce449151128fd0b12f9fde359659af1
Joshua Sandoval <joshua.sandoval031@gmail.com>
2026-01-03 16:57:38 +0800

    fix video and router

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,13 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import LandingPage from '@/views/LandingPage.vue';
-import MainPage from '@/views/MainPage.vue';
+import { createRouter, createWebHashHistory } from 'vue-router'
+import LandingPage from '@/views/LandingPage.vue'
+import MainPage from '@/views/MainPage.vue'
 
 const routes = [
-  {
-    path: '/',
-    component: LandingPage,
-  },
-  {
-    path: '/content',
-    component: MainPage,
-  },
-];
+  { path: '/', name: 'Landing', component: LandingPage },
+  { path: '/content', name: 'Content', component: MainPage },
+]
 
 export default createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL), // ensures base path is correct
-  routes,
-});
+  history: createWebHashHistory(),
+  routes
+})
